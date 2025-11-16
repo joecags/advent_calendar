40659cd4c4c6075168bbfd423dceb107ee6c7fe1
joecags <joewcagney@gmail.com>
2025-11-17 08:52:40 +1100

    Update sw.js for GitHub pages

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
 // sw.js
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("v3").then(c => c.addAll(["/", "/index.html", "/app.js", "/style.css", "/manifest.json"]))
+const CACHE_NAME = "advent-calendar-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./style.css",
+  "./manifest.json" //change name here if your file is different
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
-
